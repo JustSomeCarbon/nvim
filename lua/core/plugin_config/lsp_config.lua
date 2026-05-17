5ce18946a1ae2c6c919bf44991dd0e549703b676
Noe Garcia <garcia.noav@gmail.com>
2026-05-17 12:22:47 -0600

    feat: update mason lsp config

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,3 +1,18 @@
+local lspconfig = require('lspconfig')
+
+local lsp_attach = function(_, bufnr)
+  local opts = {buffer = bufnr}
+  -- keymappings for the lsp features
+  vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+  vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+  vim.keymap.set('n', 'gD', "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+  vim.keymap.set('n', 'gi', "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+  vim.keymap.set('n', 'go', "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+  vim.keymap.set('n', 'gr', "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+  vim.keymap.set('n', 'gs', "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+end
+
+
 -- Load Mason
 require('mason').setup({
   log_level = vim.log.levels.DEBUG
@@ -52,15 +67,4 @@ for _, server in ipairs(installed) do
 end
 vim.lsp.enable(installed)
 
-local lsp_attach = function(_, bufnr)
-  local opts = {buffer = bufnr}
-  -- keymappings for the lsp features
-  vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-  vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-  vim.keymap.set('n', 'gD', "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-  vim.keymap.set('n', 'gi', "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-  vim.keymap.set('n', 'go', "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-  vim.keymap.set('n', 'gr', "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-  vim.keymap.set('n', 'gs', "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-end
 
